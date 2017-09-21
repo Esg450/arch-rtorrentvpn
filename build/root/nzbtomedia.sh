6fabc747d91a4ad888691ed4525ba2050ea39f79
Evan Gentis <evangentis@Evans-MacBook-Pro.local>
2017-09-20 21:48:26 -0400

    fix

--- build/root/nzbtomedia.sh
+++ build/root/nzbtomedia.sh
@@ -8,10 +8,6 @@ echo "Installing nzbToMedia"
 
 GIT_APP_FOLDER="/etc/webapps/nzbToMedia"
 CONF_APP_FOLDER="/etc/webapps/nzbToMedia/config"
-
-mkdir -p $GIT_APP_FOLDER
-mkdir -p $CONF_APP_FOLDER
-
 if [[ -d "$GIT_APP_FOLDER/.git" ]]
 then
   git -C $GIT_APP_FOLDER pull
@@ -19,6 +15,7 @@ else
   git clone https://github.com/clinton-hall/nzbToMedia.git $GIT_APP_FOLDER
 fi
 
+mkdir -p $CONF_APP_FOLDER
 [[ ! -f "$CONF_APP_FOLDER/autoProcessMedia.cfg" ]] && cp $GIT_APP_FOLDER/autoProcessMedia.cfg.spec $CONF_APP_FOLDER/autoProcessMedia.cfg
 [[ ! -h "$GIT_APP_FOLDER/autoProcessMedia.cfg" ]] && ln -s $CONF_APP_FOLDER/autoProcessMedia.cfg $GIT_APP_FOLDER/autoProcessMedia.cfg
 [[ ! -f "$GIT_APP_FOLDER/logs/nzbtomedia.log" ]] && { mkdir -p $GIT_APP_FOLDER/logs && touch $GIT_APP_FOLDER/logs/nzbtomedia.log; }
