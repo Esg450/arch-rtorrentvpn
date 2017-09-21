96f599ce6c4caba84a33e486b50a56bdb5fe31c1
Evan Gentis <evangentis@Evans-MacBook-Pro.local>
2017-09-20 20:45:00 -0400

    fix

--- build/root/nzbtomedia.sh
+++ build/root/nzbtomedia.sh
@@ -1,28 +1,23 @@
-#!/usr/bin/with-contenv bash
+#!/bin/bash
 
 # exit script if return code != 0
 set -e
 
 echo "Installing nzbToMedia"
 
-git -C /app clone -q  https://github.com/clinton-hall/nzbToMedia.git
 
 GIT_APP_FOLDER="/app/nzbToMedia"
 CONF_APP_FOLDER="/config/nzbToMedia"
+if [[ -d "$GIT_APP_FOLDER/.git" ]]
+then
+  git -C $GIT_APP_FOLDER pull
+else
+  git clone https://github.com/clinton-hall/nzbToMedia.git $GIT_APP_FOLDER
+fi
 
-# make folders
-mkdir -p \
-	$CONF_APP_FOLDER
-
-# NzbToMedia config file doesn't exist
+mkdir -p $CONF_APP_FOLDER
 [[ ! -f "$CONF_APP_FOLDER/autoProcessMedia.cfg" ]] && cp $GIT_APP_FOLDER/autoProcessMedia.cfg.spec $CONF_APP_FOLDER/autoProcessMedia.cfg
-
-# NzbToMedia Config files exist but not available for the app
 [[ ! -h "$GIT_APP_FOLDER/autoProcessMedia.cfg" ]] && ln -s $CONF_APP_FOLDER/autoProcessMedia.cfg $GIT_APP_FOLDER/autoProcessMedia.cfg
-
-# Setup log file
 [[ ! -f "$GIT_APP_FOLDER/logs/nzbtomedia.log" ]] && { mkdir -p $GIT_APP_FOLDER/logs && touch $GIT_APP_FOLDER/logs/nzbtomedia.log; }
 
-chown -R abc.abc \
-	$CONF_APP_FOLDER \
-	$GIT_APP_FOLDER
+chown -R abc.abc /config /app
